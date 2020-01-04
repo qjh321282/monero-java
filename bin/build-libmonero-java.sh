6b4c13cd384b1aa005dcc277c358afdabf9eeb8b
woodser <woodser@protonmail.com>
2020-01-04 10:26:56 -0500

    wip update cmakelists - aes feature

--- bin/build-libmonero-java.sh
+++ bin/build-libmonero-java.sh
@@ -2,16 +2,9 @@
 
 #EMCC_DEBUG=1 
 
-# Make libmonero-cpp.dylib
-cd ./external/monero-cpp-library/ && 
-./bin/build-libmonero-cpp.sh &&
-
-# Copy libmonero-cpp.dylib to ./build
-cd ../../ &&
-mkdir -p ./build &&
-cp ./external/monero-cpp-library/build/libmonero-cpp.dylib ./build &&
 
 # Make libmonero-java.dylib
+mkdir -p ./build &&
 cd build && 
 cmake .. && 
 cmake --build . && 
